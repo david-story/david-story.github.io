f586302343834f64b44cf37648520d356650b087
david-story <story.a.david@gmail.com>
2018-12-07 08:07:59 -0800

    Pushing Updated Site

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Letter to the Internet",
+        "excerpt":"Greetings, Internet! Hello readers of the Internet! I am creating this post just as a formal welcome to my page, as well as testing if this Jekyll static page creation really works! What you should be able to expect from this site is to be an open-source of information and...","categories": [],
+        "tags": [],
+        "url": "/Letter-to-the-reader/",
+        "teaser":null}]
